031c151146d23a551cb636b0d202c66b2b907851
vanporman <vanporman@gmail.com>
2017-05-25 16:58:21 +0300

    injection turvad tehtud, teadanded tehtud

--- functions.php
+++ functions.php
@@ -20,12 +20,12 @@ function logi(){
     $rol = '';
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         if (isset($_POST['username']) && $_POST['username'] != ''){
-            $usr = mysqli_real_escape_string($connection, $_POST['username']);
+            $usr = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['username']));
         } else {
             $errors[] = "Kasutajanimi on puudu!";
         }
         if (isset($_POST['password']) && $_POST['password'] != ''){
-            $psw = mysqli_real_escape_string($connection, $_POST['password']);
+            $psw = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['password']));
         } else {
             $errors[] = "Parool on puudu!";
         }
@@ -68,16 +68,16 @@ function showOrders(){
     $oS = '';
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         if (isset($_POST['customerName']) && $_POST['customerName'] != ""){
-            $cN = htmlspecialchars($_POST['customerName']);
+            $cN = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['customerName']));
         }
         if (isset($_POST['daterange']) && $_POST['daterange'] != ""){
             $dR = htmlspecialchars($_POST['daterange']);
         }
         if (isset($_POST['whoIsResponsible']) && $_POST['whoIsResponsible'] != ""){
-            $wIR = htmlspecialchars($_POST['whoIsResponsible']);
+            $wIR = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['whoIsResponsible']));
         }
         if (isset($_POST['orderStatus']) && $_POST['orderStatus'] != ""){
-            $oS = htmlspecialchars($_POST['orderStatus']);
+            $oS = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['orderStatus']));
         }
     }
 
@@ -155,31 +155,31 @@ function insertOrders(){
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         if (isset($_POST['customerName']) && $_POST['customerName'] != ""){
-            $customerName = htmlspecialchars($_POST['customerName']);
+            $customerName = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['customerName']));
         }
         if (isset($_POST['singledate']) && $_POST['singledate'] != ""){
             $orderDate = htmlspecialchars($_POST['singledate']);
         }
         if (isset($_POST['orderAmount']) && $_POST['orderAmount'] != ""){
-            $orderAmount = htmlspecialchars($_POST['orderAmount']);
+            $orderAmount = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['orderAmount']));
         }
         if (isset($_POST['priceOfItem']) && $_POST['priceOfItem'] != ""){
-            $priceOfItem = htmlspecialchars($_POST['priceOfItem']);
+            $priceOfItem = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['priceOfItem']));
         }
         if (isset($_POST['sumOfOrder']) && $_POST['sumOfOrder'] != ""){
-            $sumOfOrder = htmlspecialchars($_POST['sumOfOrder']);
+            $sumOfOrder = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['sumOfOrder']));
         }
         if (isset($_POST['sizeOfPack']) && $_POST['sizeOfPack'] != ""){
-            $sizeOfPack = htmlspecialchars($_POST['sizeOfPack']);
+            $sizeOfPack = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['sizeOfPack']));
         }
         if (isset($_POST['orderStatus']) && $_POST['orderStatus'] != ""){
-            $orderStatus = htmlspecialchars($_POST['orderStatus']);
+            $orderStatus = htmlspecialchars(mysqli_escape_string($connection, $_POST['orderStatus']));
         }
         if (isset($_POST['whoIsResponsible']) && $_POST['whoIsResponsible'] != ""){
-            $whoIsResponsible = htmlspecialchars($_POST['whoIsResponsible']);
+            $whoIsResponsible = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['whoIsResponsible']));
         }
         if (isset($_POST['orderComments']) && $_POST['orderComments'] != ""){
-            $orderComments = htmlspecialchars($_POST['orderComments']);
+            $orderComments = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['orderComments']));
         }
     }
 
@@ -195,8 +195,17 @@ function insertOrders(){
                                                 '$orderComments')";
 
         $result = mysqli_query($connection, $query) or die("$query - " . mysqli_error($connection));
+
+        $row_id = mysqli_insert_id($connection);
+
+        if (isset($row_id)){
+            $confirm = " - tellimus on lisatud!";
+        }
+
         mysqli_close($connection);
     }
+
+
 
     include_once ('views/orders.html');
 }
@@ -212,7 +221,7 @@ function sale(){
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (isset($_POST['customerName']) && $_POST['customerName'] != "") {
-            $cN = htmlspecialchars($_POST['customerName']);
+            $cN = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['customerName']));
         }
         if (isset($_POST['daterange']) && $_POST['daterange'] != "") {
             $dR = htmlspecialchars($_POST['daterange']);
@@ -229,7 +238,7 @@ function sale(){
 
     $result = mysqli_query($connection, $query);
     $value = mysqli_fetch_assoc($result);
-    $myrow = $value;
+//    $myrow = $value;
 
 //    while ($row = mysqli_fetch_assoc($result)){
 //        $changeOrders[] = $row;
@@ -247,13 +256,13 @@ function makeSale(){
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (isset($_POST['id']) && $_POST['id'] != ''){
-            $id = mysqli_real_escape_string($connection, $_POST['id']);
+            $id = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['id']));
         }
         if (isset($_POST['singledate']) && $_POST['singledate'] != ""){
             $sD = htmlspecialchars($_POST['singledate']);
         }
         if (isset($_POST['statusOfOrder']) && $_POST['statusOfOrder'] != ""){
-            $sOO = htmlspecialchars($_POST['statusOfOrder']);
+            $sOO = htmlspecialchars(mysqli_real_escape_string($connection, $_POST['statusOfOrder']));
         } else {
             $query1 = "SELECT orderStatus FROM vanporman_orders WHERE orderID = '$id'";
             $result1 = mysqli_query($connection, $query1);
@@ -276,7 +285,6 @@ function makeSale(){
             $ourSales[] = $row;
         };
     }
-
 
     include_once ('views/sales.html');
 }
